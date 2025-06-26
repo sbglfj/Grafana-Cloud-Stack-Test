2b3a00f0fcf669589e6a8bad552de5fdbb6d15a3
sbglfj <jpx2b1@gmail.com>
2025-06-26 12:57:39 -0400

    Create provision-slo-resources.tf

---
+++ provision-slo-resources.tf
@@ -0,0 +1,52 @@
+// source:  https://grafana.com/docs/grafana-cloud/alerting-and-irm/slo/set-up/terraform/
+
+resource "grafana_slo" "ratio" {
+  name        = "HTTP Requests Kubelet Success Rate Indicator"
+  description = "99.5% of  Kubelet HTTP Requests are not 5xx errors"
+  query {
+    ratio {
+      success_metric  = "kubelet_http_requests_total{status!~\"5..\"}"
+      total_metric    = "kubelet_http_requests_total"
+      group_by_labels = ["job", "instance"]
+    }
+    type = "ratio"
+  }
+  objectives {
+    value  = 0.995
+    window = "30d"
+  }
+   destination_datasource { 
+      uid = "grafanacloud-prom" 
+  }
+  label {
+    key   = "team_name"
+    value = "k8s-platform"
+  }
+  label {
+    key   = "service_name"
+    value = "kubelet"
+  }
+  alerting {
+    fastburn {
+      annotation {
+        key   = "name"
+        value = "SLO Burn Rate Very High"
+      }
+      annotation {
+        key   = "description"
+        value = "Error budget is burning too fast"
+      }
+    }
+
+    slowburn {
+      annotation {
+        key   = "name"
+        value = "SLO Burn Rate High"
+      }
+      annotation {
+        key   = "description"
+        value = "Error budget is burning too fast"
+      }
+    }
+  }
+}
