e840a16ba9f5fa9c9486d14e93be152d01a6a964
sbglfj <jpx2b1@gmail.com>
2025-06-26 12:42:11 -0400

    Update add-contact-points.tf

--- add-contact-points.tf
+++ add-contact-points.tf
@@ -1,6 +1,6 @@
 // source:  https://grafana.com/docs/grafana/latest/alerting/set-up/provision-alerting-resources/terraform-provisioning/
 
-resource "grafana_contact_point" "<terraform_contact_point_name>" {
+  resource "grafana_contact_point" "<terraform_contact_point_name>" {
      name = "My contact point email"
 
      email {
